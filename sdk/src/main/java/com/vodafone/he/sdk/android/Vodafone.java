caa4735dbca2f4b0eef94ce1dfaf21666b23ce92
Bartłomiej Piech <bartlomiej.piech@mobica.com>
2014-07-03 14:22:18 +0200

    added TODO descriptions

--- sdk/src/main/java/com/vodafone/he/sdk/android/Vodafone.java
+++ sdk/src/main/java/com/vodafone/he/sdk/android/Vodafone.java
@@ -2,12 +2,15 @@ package com.vodafone.he.sdk.android;
 
 public class Vodafone {
     public static void init(String appId) {
+        // TODO SDK initialization
     }
 
     public static UserDetails getUserDetails() {
+        // TODO return cached object
         return null;
     }
 
     public static void getUserDetails(UserDetailsCallback userDetailsCallback) {
+        // TODO instantiation of backend service if not running
     }
 }
