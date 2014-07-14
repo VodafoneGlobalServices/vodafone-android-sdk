55436635918067bd4518392ae34640e33c916c70
Bartłomiej Piech <bartlomiej.piech@mobica.com>
2014-07-14 15:38:59 +0200

    removed secure flag

--- sdk/src/main/java/com/vodafone/global/sdk/UserDetailsRequestParameters.java
+++ sdk/src/main/java/com/vodafone/global/sdk/UserDetailsRequestParameters.java
@@ -5,25 +5,13 @@ package com.vodafone.global.sdk;
  */
 public class UserDetailsRequestParameters {
     private final boolean smsValidation;
-    private final boolean secure;
-    private final String secureMessage;
 
-    private UserDetailsRequestParameters(boolean smsValidation, boolean secure, String secureMessage) {
+    private UserDetailsRequestParameters(boolean smsValidation) {
         this.smsValidation = smsValidation;
-        this.secure = secure;
-        this.secureMessage = secureMessage;
     }
 
     public boolean isSmsValidation() {
         return smsValidation;
-    }
-
-    public boolean isSecure() {
-        return secure;
-    }
-
-    public String getSecureMessage() {
-        return secureMessage;
     }
 
     public static Builder builder() {
@@ -35,8 +23,6 @@ public class UserDetailsRequestParameters {
      */
     public static class Builder {
         private boolean smsValidation = false;
-        private boolean secure = false;
-        private String secureMessage = "";
 
         private Builder() {
             throw new UnsupportedOperationException();
@@ -47,39 +33,8 @@ public class UserDetailsRequestParameters {
             return this;
         }
 
-        public Builder enableSecureFlow() {
-            secure = true;
-            return this;
-        }
-
-        public Builder setSecureMessage(String message) {
-            if (message == null || message.isEmpty())
-                throw new IllegalArgumentException("Secure message can't be null or empty");
-            this.secureMessage = message;
-            return this;
-        }
-
         public UserDetailsRequestParameters build() {
-            validate();
-            return new UserDetailsRequestParameters(smsValidation, secure, secureMessage);
-        }
-
-        /**
-         * Validates that options are set properly.
-         */
-        private void validate() {
-            secureMessageIsSetIfSecureFlowIsEnabled();
-        }
-
-        /**
-         * Checks is secure message was set if secure flow was enabled
-         * and throws IllegalStateException if it wasn't.
-         */
-        private void secureMessageIsSetIfSecureFlowIsEnabled() {
-            if (secure) {
-                if (secureMessage.isEmpty())
-                    throw new IllegalStateException("Secure message has to be provided if secure flow is enabled");
-            }
+            return new UserDetailsRequestParameters(smsValidation);
         }
     }
 }
