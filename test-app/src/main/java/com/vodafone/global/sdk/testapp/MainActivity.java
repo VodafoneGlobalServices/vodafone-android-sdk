13f442d819ca47da7d4e17311fe6da42256f1a9d
Bartłomiej Piech <bartlomiej.piech@mobica.com>
2014-07-14 12:56:55 +0200

    Test app project setup and basic layout

---
+++ test-app/src/main/java/com/vodafone/global/sdk/testapp/MainActivity.java
@@ -0,0 +1,12 @@
+package com.vodafone.global.sdk.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.vodafone.global.sdk.example.R.layout.main);
+    }
+}
