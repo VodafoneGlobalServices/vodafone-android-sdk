9d92058b443566a39bcbfd2eb4bff632c2beda92
Bartłomiej Piech <bartlomiej.piech@mobica.com>
2014-08-12 15:18:41 +0200

    http service

---
+++ sdk/src/main/java/com/vodafone/global/sdk/http/VodafoneService.java
@@ -0,0 +1,15 @@
+package com.vodafone.global.sdk.http;
+
+import android.app.Application;
+
+import com.octo.android.robospice.okhttp.OkHttpSpiceService;
+import com.octo.android.robospice.persistence.CacheManager;
+import com.octo.android.robospice.persistence.exception.CacheCreationException;
+
+public class VodafoneService extends OkHttpSpiceService {
+
+    @Override
+    public CacheManager createCacheManager(Application application) throws CacheCreationException {
+        return new CacheManager();
+    }
+}
