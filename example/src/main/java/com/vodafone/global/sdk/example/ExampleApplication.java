f69302344dde8b61703e403ae1e7dcefebcedb7e
Bartłomiej Piech <bartlomiej.piech@mobica.com>
2014-09-22 14:03:30 +0200

    example: named parameters for sdk initialization

--- example/src/main/java/com/vodafone/global/sdk/example/ExampleApplication.java
+++ example/src/main/java/com/vodafone/global/sdk/example/ExampleApplication.java
@@ -7,11 +7,17 @@ import com.vodafone.global.sdk.Vodafone;
 import timber.log.Timber;
 
 public class ExampleApplication extends Application {
+
+    public static final String APP_KEY = "I1OpZaPfBcI378Bt7PBhQySW5Setb8eb";
+    public static final String APP_SECRET = "k4l1RXZGqMnw2cD8";
+    public static final String BACKEND_APP_KEY = "1234";
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Vodafone.init(this, "I1OpZaPfBcI378Bt7PBhQySW5Setb8eb",
-                "k4l1RXZGqMnw2cD8", "1234");
+
+        Vodafone.init(this, APP_KEY, APP_SECRET, BACKEND_APP_KEY);
+
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
