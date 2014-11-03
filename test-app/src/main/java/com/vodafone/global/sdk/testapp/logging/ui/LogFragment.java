be2964571329b954d544a1f6d24742781cd7b86f
Bartłomiej Piech <bartlomiej.piech@mobica.com>
2014-11-03 15:58:39 +0100

    send logs action

--- test-app/src/main/java/com/vodafone/global/sdk/testapp/logging/ui/LogFragment.java
+++ test-app/src/main/java/com/vodafone/global/sdk/testapp/logging/ui/LogFragment.java
@@ -2,10 +2,7 @@ package com.vodafone.global.sdk.testapp.logging.ui;
 
 import android.app.Fragment;
 import android.app.LoaderManager;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.CursorLoader;
-import android.content.Loader;
+import android.content.*;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.*;
@@ -16,6 +13,15 @@ import butterknife.InjectView;
 import com.vodafone.global.sdk.testapp.R;
 import com.vodafone.global.sdk.testapp.logging.database.LogColumns;
 import com.vodafone.global.sdk.testapp.logging.database.LogsProvider;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import timber.log.Timber;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class LogFragment extends Fragment {
     private static final int LOADER_ID = 20;
@@ -62,9 +68,9 @@ public class LogFragment extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        final Context appContext = getActivity().getApplicationContext();
         switch (item.getItemId()) {
             case R.id.clear_logs:
-                final Context appContext = getActivity().getApplicationContext();
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -73,8 +79,53 @@ public class LogFragment extends Fragment {
                     }
                 }).run();
                 return true;
+            case R.id.action_send_logs:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendLogsViaEmail(appContext);
+                    }
+                }).run();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendLogsViaEmail(Context appContext) {
+        try {
+            List<String[]> records = readLogs(appContext);
+            String csv = createCSV(records);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/csv");
+            intent.putExtra(Intent.EXTRA_TEXT, csv);
+            String date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ").format(new Date());
+            intent.putExtra(Intent.EXTRA_SUBJECT, "seamless-id-logs-" + date + ".csv");
+            startActivity(intent);
+        } catch (Exception e) {
+            Timber.e(e, "failed sending email");
+        }
+    }
+
+    private List<String[]> readLogs(Context appContext) {
+        ContentResolver contentResolver = appContext.getContentResolver();
+        Cursor cursor = contentResolver.query(LogsProvider.Logs.LOGS, null, null, null, null);
+        List<String[]> records = new ArrayList<String[]>();
+        while (cursor.moveToNext()) {
+            String timestamp = cursor.getString(cursor.getColumnIndex(LogColumns.TIMESTAMP));
+            String level = cursor.getString(cursor.getColumnIndex(LogColumns.LEVEL));
+            String tag = cursor.getString(cursor.getColumnIndex(LogColumns.TAG));
+            String msg = cursor.getString(cursor.getColumnIndex(LogColumns.MSG));
+            records.add(new String[]{timestamp, level, tag, msg});
+        }
+        return records;
+    }
+
+    private String createCSV(List<String[]> records) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        CSVFormat format = CSVFormat.DEFAULT.withHeader("timestamp", "level", "tag", "msg");
+        CSVPrinter printer = new CSVPrinter(builder, format);
+        printer.printRecords(records);
+        return builder.toString();
     }
 
     class LoaderCallback implements LoaderManager.LoaderCallbacks<Cursor> {
